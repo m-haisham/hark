8e544720832f71ef2378b8cd1c8c2334da2f8119
Haisham <>
2026-05-22 18:05:17 +0500

    Implement 3-pane UI layout for connections and messages

--- templates/main.js
+++ templates/main.js
@@ -1,22 +1,41 @@
 Alpine.store('app', {
   connections: [],
   activeConnectionId: null,
+  messages: [],
+  activeMessage: null,
 
   init() {
     this.source = new EventSource('/sse')
 
     this.source.addEventListener('connections', (e) => {
-      this.$store.connections.items = JSON.parse(e.data)
-      if (
-        !this.activeConnectionId &&
-        this.$store.connections.items.length > 0
-      ) {
-        this.activeConnectionId = this.$store.connections.items[0].id
+      this.connections = JSON.parse(e.data)
+      if (!this.activeConnectionId && this.connections.length > 0) {
+        this.activeConnectionId = this.connections[0].id
       }
     })
 
     this.source.onerror = () => {
       console.warn('SSE connection lost, reconnecting...')
     }
+  },
+
+  get activeConnection() {
+    return (
+      this.connections.find((c) => c.id === this.activeConnectionId) || null
+    )
+  },
+
+  selectConnection(id) {
+    this.activeConnectionId = id
+    this.activeMessage = null
+  },
+
+  selectMessage(msg) {
+    this.activeMessage = msg
+  },
+
+  stateLabel(state) {
+    if (!state) return 'unknown'
+    return state.type || 'unknown'
   },
 })
