e32aaa6e304f63c0dcd49fe17f558cd2230a8091
Haisham <47662901+m-haisham@users.noreply.github.com>
2026-05-22 19:55:18 +0500

    Add main_css route and enable axum macros feature

--- src/routes/ui.rs
+++ src/routes/ui.rs
@@ -1,5 +1,23 @@
-use crate::templates::Index;
+use std::sync::Arc;
+
+use axum::{http::HeaderName, response::IntoResponse};
+
+use crate::{response::ResponseError, state::AppState, templates::Index};
+
+pub fn routes() -> axum::Router<Arc<AppState>> {
+    axum::Router::new()
+        .route("/", axum::routing::get(ui))
+        .route("/dist/output.css", axum::routing::get(main_css))
+}
 
 pub async fn ui() -> Index {
     crate::templates::Index {}
+}
+
+#[axum::debug_handler]
+pub async fn main_css() -> Result<impl IntoResponse, ResponseError> {
+    let path = std::path::Path::new("dist/output.css");
+    let css =
+        std::fs::read_to_string(path).map_err(|e| ResponseError::ServerError(eyre::eyre!(e)))?;
+    Ok(([(HeaderName::from_static("content-type"), "text/css")], css))
 }
